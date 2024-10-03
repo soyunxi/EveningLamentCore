f74caefb101943815007c799363f16ee6f49ee73
soyunxi <3494035034@qq.com>
2024-10-03 18:45:10 +0800

    update Engraving

--- src/main/java/org/yunxi/EveningLament/common/Events/EngravingEvent.java
+++ src/main/java/org/yunxi/EveningLament/common/Events/EngravingEvent.java
@@ -1,19 +1,34 @@
 package org.yunxi.EveningLament.common.Events;
 
 
+import net.minecraft.core.registries.Registries;
+import net.minecraft.data.recipes.SmithingTransformRecipeBuilder;
 import net.minecraft.network.chat.Component;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.crafting.Recipe;
+import net.minecraft.world.item.crafting.SmithingRecipe;
+import net.minecraftforge.client.event.RecipesUpdatedEvent;
+import net.minecraftforge.event.AnvilUpdateEvent;
+import net.minecraftforge.event.TagsUpdatedEvent;
 import net.minecraftforge.event.entity.living.LivingDamageEvent;
 import net.minecraftforge.event.entity.living.LivingDeathEvent;
+import net.minecraftforge.event.entity.player.PlayerEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.registries.RegisterEvent;
 import org.yunxi.EveningLament.Eveninglament;
+import org.yunxi.EveningLament.api.Engraving.Engraving;
+import org.yunxi.EveningLament.api.Register;
 import org.yunxi.EveningLament.common.Engraving.BloodMaryEngraving;
 import org.yunxi.EveningLament.common.Engraving.EngravingRegister;
+import org.yunxi.EveningLament.common.items.ItemRegister;
 import org.yunxi.EveningLament.util.EngravingHelper;
+
+import java.util.Map;
 
 @Mod.EventBusSubscriber(modid = Eveninglament.MODID, bus = Mod.EventBusSubscriber.Bus.FORGE)
 public class EngravingEvent {
@@ -24,11 +39,28 @@ public class EngravingEvent {
         float amount = event.getAmount();
         if (directEntity instanceof Player player) {
             ItemStack mainHandItem = player.getMainHandItem();
-            player.displayClientMessage(Component.nullToEmpty(Boolean.toString(EngravingHelper.hasEngraving(mainHandItem, EngravingRegister.BLOOD_MARY.get()))), false);
-            player.displayClientMessage(Component.nullToEmpty(Integer.toString(EngravingHelper.getEngravingLevel(mainHandItem, EngravingRegister.BLOOD_MARY.get()))), false);
 
             EngravingHelper.addEngraving(mainHandItem, EngravingRegister.BLOOD_MARY.get(), 2);
         }
+    }
 
+    @SubscribeEvent
+    public static void onAnvilUpdate(AnvilUpdateEvent event) {
+        ItemStack left = event.getLeft();
+        ItemStack output = left.copy();
+        ItemStack right = event.getRight();
+        int cost = 0;
+        if (right.getItem() == ItemRegister.FLOURISHING_BLOSSOM_ENGRAVING.get()) {
+            Map<Engraving, Integer> engravings = EngravingHelper.getEngravings(right);
+            for (Engraving engraving : engravings.keySet()) {
+                if (engraving.canEnchant(left)){
+                    EngravingHelper.addEngraving(output, engraving, engravings.get(engraving));
+                    cost += engraving.getGrade().getGradeLevel() * 2 * engravings.get(engraving);
+                }
+            }
+            event.setOutput(output);
+            event.setCost(cost);
+            event.setMaterialCost(1);
+        }
     }
 }
--- src/main/java/org/yunxi/EveningLament/plugin/Jei/EveningLamentJei.java
+++ src/main/java/org/yunxi/EveningLament/plugin/Jei/EveningLamentJei.java
@@ -3,8 +3,12 @@ package org.yunxi.EveningLament.plugin.Jei;
 
 import mezz.jei.api.IModPlugin;
 import mezz.jei.api.JeiPlugin;
+import mezz.jei.api.constants.VanillaTypes;
+import mezz.jei.api.registration.IRecipeRegistration;
 import mezz.jei.api.registration.ISubtypeRegistration;
+import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.ItemStack;
 import org.yunxi.EveningLament.Eveninglament;
 import org.yunxi.EveningLament.common.items.EngravingItem;
 import org.yunxi.EveningLament.common.items.ItemRegister;
@@ -19,5 +23,12 @@ public class EveningLamentJei implements IModPlugin {
     @Override
     public void registerItemSubtypes(ISubtypeRegistration registration) {
         registration.useNbtForSubtypes(ItemRegister.FLOURISHING_BLOSSOM_ENGRAVING.get());
+    }
+
+    @Override
+    public void registerRecipes(IRecipeRegistration registration) {
+        ItemStack ENGRAVING = new ItemStack(ItemRegister.FLOURISHING_BLOSSOM_ENGRAVING.get());
+        registration.addIngredientInfo(ENGRAVING, VanillaTypes.ITEM_STACK,
+                Component.translatable("jei.info." + Eveninglament.MODID + ".engraving"));
     }
 }
