373fbb8b23999c0f91e0b163b39a55e0dba28647
soyunxi <3494035034@qq.com>
2024-10-07 22:57:14 +0800

    update Engraving

--- src/main/java/org/yunxi/EveningLament/plugin/Jei/EveningLamentJei.java
+++ src/main/java/org/yunxi/EveningLament/plugin/Jei/EveningLamentJei.java
@@ -6,12 +6,22 @@ import mezz.jei.api.JeiPlugin;
 import mezz.jei.api.constants.VanillaTypes;
 import mezz.jei.api.registration.IRecipeRegistration;
 import mezz.jei.api.registration.ISubtypeRegistration;
+import net.minecraft.ChatFormatting;
 import net.minecraft.network.chat.Component;
+import net.minecraft.network.chat.MutableComponent;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.ItemStack;
+import net.minecraftforge.registries.RegistryObject;
 import org.yunxi.EveningLament.Eveninglament;
+import org.yunxi.EveningLament.api.Engraving.Engraving;
+import org.yunxi.EveningLament.common.Engraving.EngravingRegister;
 import org.yunxi.EveningLament.common.items.EngravingItem;
 import org.yunxi.EveningLament.common.items.ItemRegister;
+import org.yunxi.EveningLament.util.EngravingHelper;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
 
 @JeiPlugin
 public class EveningLamentJei implements IModPlugin {
@@ -27,8 +37,23 @@ public class EveningLamentJei implements IModPlugin {
 
     @Override
     public void registerRecipes(IRecipeRegistration registration) {
-        ItemStack ENGRAVING = new ItemStack(ItemRegister.FLOURISHING_BLOSSOM_ENGRAVING.get());
-        registration.addIngredientInfo(ENGRAVING, VanillaTypes.ITEM_STACK,
-                Component.translatable("jei.info." + Eveninglament.MODID + ".engraving"));
+        List<ItemStack> engravings = EngravingItem.getEngravings();
+        for (ItemStack engravingItem : engravings){
+            Map<Engraving, Integer> map = EngravingHelper.getEngravings(engravingItem);
+            for(Engraving engraving: map.keySet()) {
+                for (RegistryObject<Engraving> entry : EngravingRegister.ENGRAVINGS.getEntries()) {
+                    if (engraving.equals(entry.get())) {
+                        MutableComponent Grade = Component.translatable("name.engraving.grade." + engraving.getGrade().getGradeName()).append(" ").append("(").append(Integer.toString(engraving.getGrade().getGradeLevel())).append(")");
+                        registration.addIngredientInfo(engravingItem, VanillaTypes.ITEM_STACK,
+                                Component.translatable("jei.info." + Eveninglament.MODID + ".engraving"),
+                                Component.translatable("engraving." + (entry.getId().getNamespace() + "." + entry.getId().getPath())).withStyle(ChatFormatting.BLACK),
+                                Component.translatable("jei.info." + Eveninglament.MODID + ".engraving.level", map.get(engraving)),
+                                Grade,
+                                Component.translatable("engraving." + (entry.getId().getNamespace() + "." + entry.getId().getPath() + ".desc")),
+                                Component.translatable("jei.info." + Eveninglament.MODID + ".engraving.deplete", map.get(engraving) * 2 * engraving.getGrade().getGradeLevel()));
+                    }
+                }
+            }
+        }
     }
 }
